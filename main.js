f23e1a00853a816cb810dd45bf8f54c674bcd7ca
umitcanyildirimer <umitcanyildirimer@gmail.com>
2022-12-23 01:09:56 -0800

    updating comments

--- main.js
+++ main.js
@@ -13,75 +13,66 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'PAPER' & computerSelection==='PAPER') {
 
         return result = 'It is a draw';
-        // return result;
-        //console.log('game works!');
+        
     
     }
     else if (playerSelection === 'SCISSORS' & computerSelection ==='SCISSORS') {
     
         return result = 'It is a draw';
-        // return result;
-        //console.log('game works!');
+        
     
     }
     else if (playerSelection === 'ROCK' & computerSelection==='ROCK') {
     
         return result = 'It is a draw';
-        // return result;
-        //console.log('game works!');
+        
     
     }
     else if (playerSelection === 'PAPER' & computerSelection==='ROCK') {
 
         return result = 'You Won, Paper beats Rock';
-        // return result;
-        //console.log('game works!');
+        
     
     }
     else if (playerSelection === 'ROCK' & computerSelection==='PAPER') {
 
         return result= 'You Lost, Paper beats Rock';
-        // return result;
-        //console.log('game works!');
+        
     
     }
     else if (playerSelection === 'PAPER' & computerSelection==='SCISSORS') {
     
         return result = 'You lost, Scissors beats Paper';
-        // return result;
-        //console.log('game works!');
+        
     
     }
     else if (playerSelection === 'SCISSORS' & computerSelection==='PAPER') {
     
         return result = 'You Won, Scissors beats Paper';
-        // return result;
-        //console.log('game works!');
+        
     
     }
     else if (playerSelection === 'ROCK' & computerSelection==='SCISSORS') {
     
         return result = 'You Won, Rock beats Scissors';
-        // return result;
-        //console.log('game works!');
+        
     
     }
     else if (playerSelection === 'SCISSORS' & computerSelection==='ROCK') {
     
         return result = 'You Lost, Rock beats Scissors';
-        // return result;
-        //console.log('game works!');
+        
     
     }
     else {
 
         return result = 'Something went wrong!';
-        // return result;
-        //console.log('game works!');
+        
     }
 
 }
-// Let's launch the game allowing user to make a decision
+// Let's launch the game for 5 times allowing user to make a decision
+
 function game() {
     for (let i = 0; i < 5; i++) {
 
